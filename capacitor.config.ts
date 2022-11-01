15a67781c94f74620f14be2321d4f93ca02ade6f
diahml <diahmartina16@gmail.com>
2022-11-02 00:22:54 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ivana',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
